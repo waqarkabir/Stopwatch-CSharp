edc422f85e6dcdd57112536d0f4b4f047c8dcd10
waqarkabir <waqarkabir10@gmail.com>
2022-04-05 20:34:40 +0500

    TimeWatch multiple methods implemented

--- Models/Stopwatch.cs
+++ Models/Stopwatch.cs
@@ -1,7 +1,35 @@
 ﻿namespace Models
 {
-    public class Stopwatch
+    public class StopWatch
     {
+        private TimeSpan _timeTaken;
+        private DateTime _time;
+        private bool _running;
 
+        public void Start()
+        {
+            if (_running)
+            {
+                throw new InvalidOperationException("Cannot start without stopping.");
+            }
+
+            _time = DateTime.Now;
+            _running = true;
+        }
+
+        public void Stop()
+        {
+            if (!_running)
+            {
+                throw new InvalidOperationException("Cannot stop without starting.");
+            }
+
+            _running = false;
+        }
+
+        public TimeSpan TimeTook()
+        {
+            return _timeTaken += DateTime.Now - _time;
+        }
     }
 }
--- Stopwatch.ConsoleApp/Program.cs
+++ Stopwatch.ConsoleApp/Program.cs
@@ -1,20 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 #region Microsoft Implementation Provided
-using System.Diagnostics;
+//using System.Diagnostics;
 
-Console.WriteLine("Stopwatch by Waqar Kabir");
-Stopwatch stopwatch = new Stopwatch();
+//Console.WriteLine("Stopwatch by Waqar Kabir");
+//Stopwatch stopwatch = new Stopwatch();
+//Console.WriteLine("Press any key to start Stopwatch time");
+//Console.ReadKey();
+//Console.WriteLine("");
+//stopwatch.Start();
+//DateTime startTime= DateTime.Now;
+//Console.WriteLine("Time started");
+//Console.WriteLine("Press any key to stop Stopwatch time");
+//Console.ReadKey();
+//Console.WriteLine("");
+//stopwatch.Stop();
+//DateTime endTime = DateTime.Now;
+//TimeSpan timeDiff = endTime - startTime;
+//Console.WriteLine("Calculated time lapsed is: " + timeDiff);
+
+#endregion
+
+#region Implemented by Waqar Kabir
+
+//Console.WriteLine("Stopwatch by Waqar Kabir");
+//Stopwatch stopwatch = new Stopwatch();
+//Console.WriteLine("Press any key to start Stopwatch time");
+//Console.ReadKey();
+//Console.WriteLine("");
+//Console.WriteLine("Time started");
+//Console.WriteLine("Press any key to stop Stopwatch time");
+//Console.ReadKey();
+//Console.WriteLine("");
+//stopwatch.Stop();
+//Console.WriteLine("Calculated time lapsed is: " + stopwatch.Elapsed.ToString());
+
+#endregion
+using Models;
+
+StopWatch stopWatch = new StopWatch();
+
 Console.WriteLine("Press any key to start Stopwatch time");
 Console.ReadKey();
+
 Console.WriteLine("");
-stopwatch.Start();
-DateTime startTime= DateTime.Now;
+stopWatch.Start();
+
 Console.WriteLine("Time started");
 Console.WriteLine("Press any key to stop Stopwatch time");
+
 Console.ReadKey();
 Console.WriteLine("");
-stopwatch.Stop();
-DateTime endTime = DateTime.Now;
-TimeSpan timeDiff = endTime - startTime;
-Console.WriteLine("Calculated time lapsed is: " + timeDiff);
-#endregion
+
+stopWatch.Stop();
+Console.WriteLine("Calculated time lapsed is: " + stopWatch.TimeTook());
