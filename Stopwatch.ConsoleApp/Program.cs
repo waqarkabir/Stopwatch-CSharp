f8b53e927bcab992453a2e3238848c0c68d7f961
waqarkabir <waqarkabir10@gmail.com>
2022-04-05 18:03:19 +0500

    Stopwatch tested by running

--- Stopwatch.ConsoleApp/Program.cs
+++ Stopwatch.ConsoleApp/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#region Microsoft Implementation Provided
+using System.Diagnostics;
+
+Console.WriteLine("Stopwatch by Waqar Kabir");
+Stopwatch stopwatch = new Stopwatch();
+Console.WriteLine("Press any key to start Stopwatch time");
+Console.ReadKey();
+Console.WriteLine("");
+stopwatch.Start();
+DateTime startTime= DateTime.Now;
+Console.WriteLine("Time started");
+Console.WriteLine("Press any key to stop Stopwatch time");
+Console.ReadKey();
+Console.WriteLine("");
+stopwatch.Stop();
+DateTime endTime = DateTime.Now;
+TimeSpan timeDiff = endTime - startTime;
+Console.WriteLine("Calculated time lapsed is: " + timeDiff);
+#endregion
